e49517df2134a3a3268737af9060221dc71cbaf1
yminfosys <sukanta.uk@gmail.com>
2025-04-17 03:38:49 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,33 @@ const axios = require('axios');
 
 
 router.get('/', async (req, res) => {
-  try {
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    // Handle local testing
-    if (ip === '::1' || ip === '127.0.0.1') {
-      ip = '27.255.221.0'; // Example IP from India
-    }
-
-    // Clean IPv4-mapped IPv6 (e.g., ::ffff:104.248.161.110)
-    if (ip.includes('::ffff:')) {
-      ip = ip.split('::ffff:')[1];
-    }
-
-
-    const response = await axios.get(`https://ipapi.co/${ip}/json/`);
-    const country = response.data.country_name;
-
-    console.log(country, ip)
-
-    if (country === 'India') {
-      return res.redirect('https://paacrypto.com');
-    } else {
-     //return res.redirect('https://paacryptobank.com');
-     res.render('index', { country });
-    }
-  } catch (error) {
-    console.error('Geo lookup failed:', error.message);
-    return res.redirect('https://paacryptobank.com');
-  }
+  res.render('index', { title:"Paacryptobank" });
 });
 
 
+router.get('/check-location', (req, res) => {
+  const { lat, lon } = req.query;
+  const latitude = parseFloat(lat);
+  const longitude = parseFloat(lon);
+
+  let redirect;
+
+  // Simple bounding boxes (roughly)
+  if (latitude >= 6 && latitude <= 36 && longitude >= 68 && longitude <= 97) {
+    redirect = 'https://paacrypto.com'; // India
+   // redirect = '/uk';
+  } else if (latitude >= 20 && latitude <= 27 && longitude >= 88 && longitude <= 92) {
+    redirect = '/uk';
+  } else {
+    redirect = '/uk';
+  }
+
+  res.json({ redirect });
+});
+
+router.get('/uk', async (req, res) => {
+  res.render('home', { title:"Paacryptobank" });
+});
 
 
 
